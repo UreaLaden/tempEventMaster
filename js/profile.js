c093e4a94a080d3c80747d07c489eaa8cf03e626
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-04-20 22:44:14 -0400

    resized weekly calendar add jquery

--- js/profile.js
+++ js/profile.js
@@ -1,15 +1,14 @@
 var calendarEl = $('#calendar');
 
 calendarEl.fullCalendar({
-header: 
-    {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'listMonth,listWeek,listDay'
-    },
+// header: 
+//     {
+//         // left: 'prev,next today',
+//         // center: 'title',
+//         // right: 'listMonth,listWeek,listDay'
+//     },
     defaultView: 'listWeek',
     events: [
-
     {
         title: 'All Day Event',
         start: '2021-04-20'
@@ -24,3 +23,10 @@ header:
         title: 'Repeating Event',
         start: '2020-12-09T16:00:00'
     }]});
+
+    // TODO: ON CLICK SHOULD HIDE MEMBER BTN AND SHOW ADD MEMBER FORM
+ $('.member-btn').click(function(){     
+    //  $('#member-btn').hide();
+     $('#member-form').show();
+    });
+ 
